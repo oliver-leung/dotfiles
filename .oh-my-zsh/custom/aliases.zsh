d1bb9c4b3e24fec6d022af0c7867d0202beea158
Oliver Leung <oliverleung2@gmail.com>
2022-02-09 11:13:03 -0500

    Add new aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,28 +1,18 @@
-# Edit shell behavior
-alias edit-alia="nano $ZSH_CUSTOM/aliases.zsh && source $ZSH_CUSTOM/aliases.zsh"
-alias edit-path="nano ~/.profile && source ~/.profile"
-
-# Listing current directory contents
-alias ll="ls -lh"
-alias la="ls -lah"
-
 # Go to Windows locations
 alias c="/mnt/c"
-alias ghw="/mnt/c/Users/olive/GitHub"
 alias exp="explorer.exe ."
 alias chrome="/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe"
 
-# APT installation
+# Apt-get
 alias sai="sudo apt install"
 alias sar="sudo apt remove"
 alias sup="sudo apt update && sudo apt upgrade -y"
 
 # Python
-alias python="python3"
 alias ipynb="jupyter notebook --port=8889 --no-browser"
 
-# Cornell CIS Undergrad Computing Lab Servers
-alias cis-server="ssh oal22@ugclinux.cs.cornell.edu"
+# SSH hosts
+alias leungnas="ssh pi@192.168.1.148"
 
 # Git
 alias lg="lazygit"
@@ -36,3 +26,11 @@ alias pcp="rsync -r --progress"
 
 # Configure dotfile sync
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+# Better cat
+alias cat="batcat"
+
+# Better top
+alias top="bpytop"
+alias utop="bpytop"
+alias htop="bpytop"
